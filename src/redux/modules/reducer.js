725a5e70d13e9adcf73b207fb49a3f67ca2c98b0
Nicolas Bazille <oltodo@msn.com>
2016-06-13 14:01:15 +0200

    Remove unused code

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
 import { reducer as reduxAsyncConnect } from 'redux-async-connect';
 import { createResponsiveStateReducer } from 'redux-responsive';
@@ -7,25 +6,10 @@ import { createResponsiveStateReducer } from 'redux-responsive';
 import { app } from '../../config';
 
 import posts from './posts';
-import auth from './auth';
-import counter from './counter';
-import {reducer as form} from 'redux-form';
-import info from './info';
-import widgets from './widgets';
 
 export default combineReducers({
   routing: routerReducer,
   reduxAsyncConnect,
   browser: createResponsiveStateReducer(app.breakpoints),
   posts,
-
-  auth,
-  form,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
-  info,
-  widgets
 });
