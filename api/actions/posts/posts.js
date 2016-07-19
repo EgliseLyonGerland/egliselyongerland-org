5c1bcc1de616aa41d44aa8754f556dbb1c73c00c
Nicolas Bazille <oltodo@msn.com>
2016-07-19 15:11:57 +0200

    Fix categories filter on API
    - Add more posts

--- api/actions/posts/posts.js
+++ api/actions/posts/posts.js
@@ -1,13 +1,54 @@
 import data from './data';
 
+import { get, find, indexOf, random } from 'lodash';
+
+function postHasAllCagtegories(post, categoriesID) {
+  const postCategories = get(post, 'terms.category', []);
+
+  const categoriesMatched = postCategories.reduce((total, category) => {
+    if (indexOf(categoriesID, category.ID + '') > -1) {
+      return total + 1;
+    }
+
+    return total;
+  }, 0);
+
+  return categoriesMatched == categoriesID.length;
+}
+
 export default function posts({ query }) {
   return new Promise((resolve, reject) => {
     const {
-      limit = 100,
-      offset = 0 } = query;
+      page = 1,
+      filter = {},
+    } = query;
 
-    let results = data.slice(offset, limit);
+    const {
+      posts_per_page = 15,
+      category__and,
+    } = filter;
 
-    resolve(results);
+    let results = data;
+
+    // Filter by categories
+    if (category__and) {
+      results = results.reduce((previous, current) => {
+        if (postHasAllCagtegories(current, category__and)) {
+          previous.push(current);
+        }
+
+        return previous;
+      }, []);
+    }
+
+    // Pagination
+    results = results.slice(
+      ((page - 1) * posts_per_page),
+      (page * posts_per_page)
+    );
+
+    setTimeout(() => {
+      resolve(results);
+    }, random(0, 200) * 10);
   });
 }
