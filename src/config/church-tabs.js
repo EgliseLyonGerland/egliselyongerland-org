4a388ad883e971ad2b4c77b35d2eaea9bdc2a765
Nicolas Bazille <nicolas.bazille@m6.fr>
2017-09-06 23:17:18 +0200

    Change church pages header image

--- src/config/church-tabs.js
+++ src/config/church-tabs.js
@@ -1,40 +1,47 @@
 import { ChurchAboutUs, ChurchWip } from "containers";
 
+import aboutUsPicture from "containers/Church/jumbotrons/about-us.jpg";
+import activityPicture from "containers/Church/jumbotrons/activity.jpg";
+import ourFaithPicture from "containers/Church/jumbotrons/our-faith.jpg";
+import worshipPicture from "containers/Church/jumbotrons/worship.jpg";
+import teamPicture from "containers/Church/jumbotrons/team.jpg";
+import federativeLinksPicture from "containers/Church/jumbotrons/federative-links.jpg";
+
 export default [
   {
     slug: "qui-sommes-nous",
     title: "Qui sommes-nous ?",
-    picture: "http://lorempixel.com/output/people-q-c-1280-720-1.jpg",
+    picture: aboutUsPicture,
     component: ChurchWip
   },
   {
     slug: "que-croyons-nous",
     title: "Que croyons-nous ?",
-    picture: "http://lorempixel.com/output/nightlife-q-c-1280-720-7.jpg",
+    picture: ourFaithPicture,
     component: ChurchWip
   },
   {
     slug: "le-culte",
     title: "Le culte",
-    picture: "http://lorempixel.com/output/sports-q-c-1280-720-5.jpg",
+    picture: worshipPicture,
     component: ChurchWip
   },
   {
     slug: "activites",
     title: "Activités",
-    picture: "http://lorempixel.com/output/nature-q-c-1280-720-2.jpg",
+    picture: activityPicture,
     component: ChurchWip
   },
   {
     slug: "l-equipe",
     title: "L'équipe",
-    picture: "http://lorempixel.com/output/nature-q-c-1280-720-3.jpg",
+    picture: teamPicture,
     component: ChurchWip
   },
   {
     slug: "liens-federatifs",
     title: "Liens fédératifs",
-    picture: "http://lorempixel.com/output/food-q-c-1280-720-9.jpg",
+    picture: federativeLinksPicture,
     component: ChurchWip
   }
 ];
