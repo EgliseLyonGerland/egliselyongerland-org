a605e5189c122bd3128d3260af04a3e19959ee9b
Nicolas Bazille <nicolas.bazille@m6.fr>
2018-04-08 22:46:30 +0200

    Move WhatWhenWhere compoenent styles to JSS

--- src/components/WhatWhenWhere/WhatWhenWhere.js
+++ src/components/WhatWhenWhere/WhatWhenWhere.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import { withStyles } from "material-ui";
 
 import { Link } from "react-router";
 
@@ -6,33 +7,42 @@ import { Image, Text, H2, Hr } from "components";
 import Button from "components/Button/Button";
 import routes from "utils/routes";
 
-import styles from "./WhatWhenWhere.scss";
-
-class WhatWhenWhere extends Component {
-  render() {
-    return (
-      <div className={styles.www}>
-        <div className={styles.right}>
-          <H2>
-            Culte ouvert à tous,<br /> le dimanche à 17h.
-          </H2>
-          <Text>302 avenue Jean Jaurès</Text>
-          <Text>69007 Lyon</Text>
-          <Hr lg />
-          <Link to={routes.worship()}>
-            <Button bordered>En savoir plus</Button>
-          </Link>
-          <Hr inline />
-          <Link to={routes.contact()}>
-            <Button bordered>Contact</Button>
-          </Link>
-        </div>
-        <div className={styles.left}>
-          <Image src="/images/map.png" height={450} />
-        </div>
-      </div>
-    );
+const styles = {
+  www: {
+    background: "#eee",
+    display: "flex",
+    alignItems: "center"
+  },
+  right: {
+    width: 450,
+    padding: 30
+  },
+  left: {
+    flexGrow: 1
   }
-}
+};
 
-export default WhatWhenWhere;
+const WhatWhenWhere = ({ classes }) => (
+  <div className={classes.www}>
+    <div className={classes.right}>
+      <H2>
+        Culte ouvert à tous,<br /> le dimanche à 17h.
+      </H2>
+      <Text>302 avenue Jean Jaurès</Text>
+      <Text>69007 Lyon</Text>
+      <Hr lg />
+      <Link to={routes.worship()}>
+        <Button bordered>En savoir plus</Button>
+      </Link>
+      <Hr inline />
+      <Link to={routes.contact()}>
+        <Button bordered>Contact</Button>
+      </Link>
+    </div>
+    <div className={classes.left}>
+      <Image src="/images/map.png" height={450} />
+    </div>
+  </div>
+);
+
+export default withStyles(styles)(WhatWhenWhere);
