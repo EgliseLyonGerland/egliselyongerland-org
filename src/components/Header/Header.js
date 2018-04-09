2f1d332590ebacd16a6b9fcab6b94a0f61025af9
Nicolas Bazille <nicolas.bazille@m6.fr>
2018-04-09 17:59:04 +0200

    Move Header compoenent styles to JSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,124 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router";
-import classes from "classnames";
+import classnames from "classnames";
 import EventListener, { withOptions } from "react-event-listener";
+import { withStyles } from "material-ui";
 
 import { Sidebar, /* SearchButton, */ Burger, Container } from "components";
 import routes from "utils/routes";
 
-import styles from "./Header.scss";
 import logo from "./logo.svg";
 import brand from "./brand.svg";
+
+const styles = theme => ({
+  header: {
+    position: "fixed",
+    top: 0,
+    zIndex: theme.header.zindex,
+    width: "100%",
+    transition: "background 0.3s"
+  },
+  mini: {
+    "& $brand": {
+      transform: `scale(${theme.header.sticky.brandScale})
+          translateX(${theme.header.sticky.brandTranslate}px)`
+    }
+  },
+  sticky: {
+    background: "#111",
+
+    "& $body": {
+      height: theme.header.sticky.height
+    },
+    "& $brand": {
+      transform: `scale(${theme.header.sticky.brandScale})
+          translateX(${theme.header.sticky.brandTranslate}px)`
+    },
+    "& $logo": {
+      transform: "rotate(180deg)"
+    }
+  },
+  body: {
+    display: "flex",
+    alignItems: "center",
+    height: theme.header.height,
+    padding: [[0, 30]],
+    transition: "height 0.3s"
+  },
+  brand: {
+    transition: "transform 0.3s",
+    transformOrigin: "left center",
+    position: "relative"
+  },
+  logo: {
+    position: "relative",
+    display: "inline-block",
+    marginRight: 20,
+    transition: "transform 0.5s",
+
+    "&, & > img": {
+      height: theme.header.logo.height
+    }
+  },
+  betaMark: {
+    display: "block",
+    background: "#e83c3c",
+    borderRadius: 3,
+    color: "white",
+    padding: [[2, 4]],
+    position: "absolute",
+    left: theme.header.logo.height - 16,
+    top: 0,
+    fontSize: 9,
+    fontWeight: 400,
+    lineHeight: "1em",
+    width: 30,
+    textAlign: "center",
+    cursor: "default",
+    textTransform: "uppercase"
+  },
+  title: {
+    height: theme.header.brand.height,
+    marginRight: 20
+  },
+  burger: {
+    position: "relative",
+    marginLeft: 35,
+    zIndex: theme.sidebar.zindex + 1
+  },
+  search: {
+    display: "none",
+    marginLeft: 20
+  },
+  blankItem: {
+    marginLeft: "auto"
+  },
+  links: {
+    display: "flex",
+    flexGrow: 0
+  },
+  linksItem: {
+    flexGrow: 0,
+    textAlign: "right",
+    marginLeft: 20
+  },
+  link: {
+    color: "white",
+    textDecoration: "none",
+    textTransform: "uppercase",
+    fontWeight: theme.typography.fontWeights.normal,
+    fontSize: 16,
+
+    "&:hover": {
+      textDecoration: "underline"
+    },
+    "&:focus, &:visited": {
+      color: "white",
+      textDecoration: "none"
+    }
+  }
+});
 
 const links = [
   {
@@ -77,15 +186,16 @@ class Header extends Component {
 
     const {
       browser,
+      classes,
       sidebarOpened,
       // onSearchButtonClicked,
       onOpenSidebarButtonClicked,
       onCloseSidebarButtonClicked
     } = this.props;
 
-    const className = classes(styles.header, {
-      [styles.sticky]: sticky,
-      [styles.mini]: sticky || browser.width <= 960
+    const className = classnames(classes.header, {
+      [classes.sticky]: sticky,
+      [classes.mini]: sticky || browser.width <= 960
     });
 
     return (
@@ -99,16 +209,16 @@ class Header extends Component {
           })}
         />
 
-        <Container className={styles.body}>
-          <div className={styles.brand}>
-            <Link className={styles.logo} to="/">
+        <Container className={classes.body}>
+          <div className={classes.brand}>
+            <Link className={classes.logo} to="/">
               <img src={logo} alt="Église Lyon Gerland" />
             </Link>
 
             {browser.width >= 600 && (
               <Link to="/">
                 <img
-                  className={styles.title}
+                  className={classes.title}
                   src={brand}
                   height="22"
                   alt="Église Lyon Gerland"
@@ -117,20 +227,20 @@ class Header extends Component {
             )}
 
             <div
-              className={styles.betaMark}
+              className={classes.betaMark}
               title="Ce site internet est en cours de perfectionnement. Il se peut que vous rencontriez des problèmes au cours de votre navigation et nous vous prions de nous en excuser."
             >
               beta
             </div>
           </div>
 
-          <div className={styles.blankItem} />
+          <div className={classes.blankItem} />
 
           {browser.width >= 930 ? (
-            <div className={styles.links}>
+            <div className={classes.links}>
               {links.map((link, index) => (
-                <div className={styles.linksItem} key={index}>
-                  <Link className={styles.link} to={link.path}>
+                <div className={classes.linksItem} key={index}>
+                  <Link className={classes.link} to={link.path}>
                     {link.label}
                   </Link>
                 </div>
@@ -138,7 +248,7 @@ class Header extends Component {
             </div>
           ) : (
             <button
-              className={styles.burger}
+              className={classes.burger}
               onClick={() => this.toggleSidebar()}
             >
               <Burger
@@ -152,7 +262,7 @@ class Header extends Component {
             </button>
           )}
 
-          {/* <div className={styles.search}>
+          {/* <div className={classes.search}>
             <SearchButton onClicked={() => onSearchButtonClicked()} />
           </div> */}
         </Container>
@@ -170,4 +280,4 @@ class Header extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Header);
+export default withStyles(styles)(connect(mapStateToProps)(Header));
