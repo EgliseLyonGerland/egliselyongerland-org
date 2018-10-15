0f43c89153d12897301ef9bd7e8615b7f58b119d
Nicolas Bazille <nicolas.bazille@m6.fr>
2018-10-15 11:01:47 +0200

    Add webpack analyzer

--- webpack/client.dev.js
+++ webpack/client.dev.js
@@ -1,10 +1,15 @@
 const webpack = require('webpack');
 const WriteFileWebpackPlugin = require('write-file-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const baseConfig = require('./client.base');
 
 const config = {
   ...baseConfig,
   plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
     new WriteFileWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     ...baseConfig.plugins,
