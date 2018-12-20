a020dee82538e71b124964208416d0b4de31539a
Nicolas Bazille <nicolas.bazille@m6.fr>
2018-12-20 07:35:41 +0100

    fixup

--- src/shared/containers/Church/Staff.js
+++ src/shared/containers/Church/Staff.js
@@ -14,15 +14,14 @@ const Staff = () => (
         <b>Pasteur</b>
       </Typography>
       <Typography paragraph>
-        Alexandre est diplômé en musicologie (Université Lumière{' '}
+        Je suis diplômé en musicologie (Université Lumière{' '}
         <span style={{ whiteSpace: 'nowrap' }}>Lyon 2</span>) et en théologie
-        (Faculté Jean Calvin, Aix-en-Provence), il éprouve beaucoup de joie à
-        étudier la Bible et à en partager le message avec d'autres. Ses
-        passe-temps favoris ?
+        (Faculté Jean Calvin, Aix-en-Provence), j'éprouve beaucoup de joie à
+        étudier la Bible et à en partager le message avec d'autres.
       </Typography>
       <Typography>
-        L'escalade, la randonnée, ou le ski ! Alexandre est le mari (privilégié)
-        de Suzanne, et le père (débordé) de six enfants.
+        Mes passe-temps favoris ? L'escalade, la randonnée, ou le ski ! Je suis
+        le mari (privilégié) de Suzanne, et le père (débordé) de six enfants.
       </Typography>
     </Elder>
     <Elder name="Jonah Haddad" picture={jonahPicture} inverted>
@@ -30,14 +29,14 @@ const Staff = () => (
         <b>Ancien</b>
       </Typography>
       <Typography paragraph>
-        Jonah a fait des études de philosophie qui l'ont conduit jusqu'à la
-        maîtrise, puis il a fait un master en théologie à distance avec la
-        Faculté de Théologie Évangélique de Louvain, en Belgique. Il prépare
+        J'ai fait des études de philosophie qui m'ont conduit jusqu'à la
+        maîtrise, puis j'ai fait un master en théologie à distance avec la
+        Faculté de Théologie Évangélique de Louvain, en Belgique. je prépare
         maintenant un doctorat.
       </Typography>
       <Typography>
-        Jonah est le mari d'Amy, avec qui il a quatre enfants. Il aime étudier,
-        prêcher à l’église, et dans son temps libre, faire du sport. Jonah a un
+        Je suis le mari d'Amy, avec qui j'ai quatre enfants. J'aime étudier,
+        prêcher à l’église, et dans mon temps libre, faire du sport. J'ai un
         petit faible pour les randonnées dans la nature.
       </Typography>
     </Elder>
@@ -46,13 +45,13 @@ const Staff = () => (
         <b>Ancien</b>
       </Typography>
       <Typography paragraph>
-        Denis est médecin généraliste, parfois en cabinet, parfois aux urgences.
-        Il est marié avec Maïlys avec qui il s'occupe du groupe d'ados. Il aime
+        Je suis médecin généraliste, parfois en cabinet, parfois aux urgences.
+        Je suis marié avec Maïlys avec qui je m'occupe du groupe d'ados. j'aime
         étudier la Bible et rendre son message accessible à tous.
       </Typography>
       <Typography>
-        Il aime aussi beaucoup la montagne. Il n'est pas rare qu'entre deux
-        gardes, Denis enfile vite ses chaussons d'escalade et aille grimper !
+        J'aime aussi beaucoup la montagne. Il n'est pas rare qu'entre deux
+        gardes, j'enfile vite mes chaussons d'escalade et aille grimper !
       </Typography>
     </Elder>
   </>
